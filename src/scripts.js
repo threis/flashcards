e1a067700345e620cdfa3cc02a04af8322fc563b
Thiago Reis <thiagoreis93@gmail.com>
2022-04-02 12:23:25 -0300

    add: json-server and function to persiste data from flashcards inputs

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,24 @@
-function handleAddFlashCards(){
-    
+function handleAddFlashCards(event) {
+
+    event.preventDefault()
+
     const inputFront = document.querySelector('form')
     const front = inputFront.front.value
 
     const inputBack = document.querySelector('form')
     const back = inputBack.back.value
-    
+
+    fetch("http://localhost:3000/flashcards", {
+        method: "POST",
+        body: JSON.stringify({
+            front,
+            back
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+
+        }
+    }).then(document.querySelector('form').front.value = '')
+        .then(document.querySelector('form').back.value = '')
+        .catch(err => console.log(`Error: ${err}`));
 }
