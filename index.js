52f47a2adfb5f45d23c38d5221116a75fd0ef2e3
Thiago Reis <thiagoreis93@gmail.com>
2022-04-03 14:40:27 -0300

    add: ipcMain and ipcRenderer and changed flashcards-add-page to another window

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      
-    })
-  
-    win.loadFile('src/index.html')
-  }
+let mainWindow
+let addFlashcardsWindow
 
-  app.whenReady().then(() => {
-    createWindow()
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false
+    }
   })
+  mainWindow.loadFile('src/index.html')
+  mainWindow.webContents.openDevTools()
+
+  addFlashcardsWindow = new BrowserWindow({
+    width: 400,
+    height: 400,
+    show: false
+  })
+  addFlashcardsWindow.loadFile('src/addFlashCards.html')
+
+
+  ipcMain.on('open-add-flashcards', () => {
+    addFlashcardsWindow.show()
+  })
+
+  ipcMain.on('close-add-flashcards', () => {
+    addFlashcardsWindow.hide()
+  })
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
